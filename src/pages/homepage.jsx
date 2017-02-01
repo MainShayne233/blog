20e3311a13f900384266a266d50ed4df62158ce3
MainShayne233 <shaynetremblay@hotmail.com>
2017-02-01 17:24:20 -0500

    adds react router and homepage

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Homepage extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>a blog about things im thinking about</p>
+        <p>not necessarily things i know a lot about</p>
+        <p>this is obviousily a create-react-app</p>
+        <a href="https://github.com/MainShayne233/blog">and open source af</a>
+      </div>
+    )
+  }
+}
